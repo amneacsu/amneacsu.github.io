f9675e1d0dc34259731d22e8c21a3f81843b470f
Andrei Neacșu <0x514@protonmail.com>
2025-05-31 14:57:21 +0300

    refactor: add package pages

--- rspack.config.js
+++ rspack.config.js
@@ -1,6 +1,24 @@
 import { rspack } from '@rspack/core';
+import fs from 'fs';
+import path from 'path';
+import { glob } from 'glob';
+
+import pack from './package.json' with { type: "json" };
+
+const packageNames = glob
+  .sync(pack.workspaces[0], { cwd: '.', absolute: true })
+  .filter((packagePath) => {
+    return fs.existsSync(path.join(packagePath, 'src', 'index.html'));
+  })
+  .map((packagePath) => path.basename(packagePath));
 
 export default {
+  entry: packageNames.reduce((acc, packageName) => {
+    return {
+      ...acc,
+      [packageName]: path.resolve('packages', packageName),
+    };
+  }, {}),
   experiments: {
     css: true,
   },
@@ -8,8 +26,12 @@ export default {
     clean: true,
   },
   plugins: [
-    new rspack.HtmlRspackPlugin({
-      template: './src/index.html',
+    ...packageNames.map((packageName) => {
+      return new rspack.HtmlRspackPlugin({
+        template: path.join('packages', packageName, 'src', 'index.html'),
+        filename: path.join(packageName === 'home' ? '' : packageName, 'index.html'),
+        chunks: [packageName],
+      });
     }),
   ],
 };
