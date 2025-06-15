faba12fcbabcdf76330368b94a22026a44b0ee8a
Andrei Neacșu <0x514@protonmail.com>
2025-06-16 01:18:22 +0300

    nuke

--- packages/home/src/components/Projects/styles.ts
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-import { Section } from '../../elements/Section.ts';
-
-export const Projects = styled(Section)`
-  background-color: hsla(58, 29%, 45%, 1);
-`;
--- packages/home/src/elements/Grid.ts
+++
@@ -1,7 +0,0 @@
-import styled from "styled-components";
-
-export const Grid = styled.div`
-  max-width: 960px;
-  margin: 0 auto;
-  padding: 0 4px;
-`;
