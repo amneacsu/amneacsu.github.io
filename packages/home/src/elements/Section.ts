05dd3aa9da4946357d6c51f6e50b29e7aefbbf3f
Andrei Neacșu <0x514@protonmail.com>
2025-06-16 01:18:22 +0300

    nuke

--- packages/home/src/elements/Section.ts
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-export const Section = styled.div`
-  position: relative;
-  z-index: 10;
-  box-sizing: border-box;
-  padding: 40px 0;
-  z-index: 2;
-`;
